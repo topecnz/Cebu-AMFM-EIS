fcba4498d0380602d120aaf454df2a6ab8c4d77b
topecnz <topecnz@gmail.com>
2023-11-16 22:46:11 +0800

    revert mysql to sqlite for admin page only

--- eis/settings.py
+++ eis/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'eis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eis_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
