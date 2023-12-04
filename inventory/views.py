4b5914f783b05e7ff85f15aa286f289f66e20d32
topecnz <topecnz@gmail.com>
2023-12-04 15:33:00 +0800

    Update various files

--- inventory/views.py
+++ inventory/views.py
@@ -14,38 +14,38 @@ def user_login(request: HttpRequest):
         u = request.POST['username']
         p = request.POST['password']
         form = UserLoginForm(request.POST)
+        message = None
+        status = None
         if form.is_valid():
             user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             if user:
                 login(request, user)
-                if request.method == 'GET':
-                    rd = request.GET['next']
-                    if rd:
-                        return redirect(rd)
-                return redirect('/')
-            obj = {
-                'message': 'Invalid Username or Password'
-            }
+                if request.user.acc_is_active:
+                    if request.method == 'GET':
+                        rd = request.GET['next']
+                        if rd:
+                            return redirect(rd)
+                    return redirect('/')
+                else:
+                    message = 'Account Removed'
+                    status = 'danger'
+                    logout(request)
             
-            return render(request, 'login.html', obj)
+        obj = {
+            'message': message if message else 'Invalid Username or Password',
+            'status': status if status else 'warning'
+        }
         
-        
+        return render(request, 'main/login.html', obj)
     
-    return render(request, "login.html", {})
-
-def add_account(request: HttpRequest):
-    
-    if request.method == 'POST':
-        pass
-    
-    return redirect('/')
+    return render(request, "main/login.html", {})
 
 @login_required(login_url='/login')
 def home(request: HttpRequest):
     obj = {
         'user': request.user
     }
-    return render(request, "index.html", obj)
+    return render(request, "main/index.html", obj)
 
 def user_logout(request: HttpRequest):
     if request.user.is_authenticated:
