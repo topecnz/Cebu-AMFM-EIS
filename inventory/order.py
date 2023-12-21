90369b4831403f28119ddbc1d8002f62454ed672
mj <148041616+yanachified@users.noreply.github.com>
2023-12-21 18:52:52 +0800

    addview_order

--- inventory/order.py
+++ inventory/order.py
@@ -8,73 +8,114 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
 from django.core.exceptions import PermissionDenied, ObjectDoesNotExist, SuspiciousOperation
-
 from inventory import customer
 from .forms import *
 from .models import Customer, Employee, AccountType, Product, ProductBrand, ProductType, Inventory, Invoice, OrderList
 
 @login_required(login_url = '/login')
 def orders(request: HttpRequest):
-    orders = OrderList.objects.all()
+    order = OrderList.objects.select_related('inv','prod')
     obj = {
-        'result' : orders,
+        'result' : order,
     }
     
     return render(request, 'main/orders.html', obj)
 
-def add_order(request: HttpRequest):
+def add_order(request):
+    products = Product.objects.all()
+    
     if not request.user.is_authenticated:
         return redirect('/')
     
     if request.user.acc_type_id != 3:
-     
-
-     return render(request, 'main/add_order.html')
+        # Generate or retrieve the latest invoice ID here
+        invoice_id = Invoice.objects.latest('id').id if Invoice.objects.exists() else 1
+        
+        return render(request, 'main/add_order.html', {'products': products, 'invoice_id': invoice_id})
     
     raise PermissionDenied()
+
 
 def view_order(request: HttpRequest, id: int):
     if not request.user.is_authenticated:
         return redirect('/')
     
-    invoice = Invoice.objects.filter(inv_id=id)
+    order = OrderList.objects.filter(ord_id=id)
 
 
-    if invoice:
-        for u in invoice:
-            invoice = u
+    if order:
+        for u in order:
+            order = u
 
         obj ={
-            'invoice': invoice
+            'order': order
         }
         return render(request, 'main/view_order.html', obj) 
     
     return render(request, 'main/view_order.html', {
-            'error': 'Invoice not found!'
+            'error': 'Order not found!'
         })
 
 @csrf_exempt
-def submit_order(request: HttpRequest):
+def submit_order(request):
     if request.user.is_authenticated:
         if request.user.acc_type_id != 3:
             if request.method == "POST":
-                i = request.POST['invDateLabel']
-                a = request.POST['amount']
-                c =  request.POST['custid']
-                e =  request.POST['empIdLabel']
+                p = request.POST['price']
+                q = request.POST['qty']
+                i = request.POST['invoice_id']
+                pro_id = request.POST['products']  # Assuming this is the ID of the product
 
-                inv = Invoice.object.create(inv_date = i, inv_amount=a)
-                inv.save()
+                # Fetch the Invoice instance based on the provided ID
+                invoice = Invoice.objects.get(pk=i)
 
+                # Fetch the Product instance based on the provided ID
+                product = Product.objects.get(pk=pro_id)
+
+                # Create the OrderList instance with the fetched instances
+                ord = OrderList.objects.create(ord_price=p, ord_qty=q, inv=invoice, prod=product)
+                
                 obj = {
-                    'code': 200 if inv else 204,
-                    'message': 'Product is successfully added!' if inv else 'Error!',
+                    'code': 200 if ord else 204,
+                    'message': 'Order is successfully added!' if ord else 'Error!',
                 }
+
                 return JsonResponse(obj)
                 
     return redirect('/')
 
 
+@csrf_exempt
+def update_order(request):
+    if request.user.is_authenticated:
+        if request.user.acc_type_id != 3:
+            if request.method == "POST":
+                id = request.POST['id']
+                p = request.POST['price']
+                q = request.POST['qty']
+                i = request.POST['invoice_id']
+                pro_id = request.POST['products']  # Assuming this is the ID of the product
 
+                # Fetch the OrderList instance based on the provided ID
+                ord = OrderList.objects.get(ord_id=id)
 
+                # Fetch the Invoice instance based on the provided ID
+                invoice = Invoice.objects.get(pk=i)
 
+                # Fetch the Product instance based on the provided ID
+                product = Product.objects.get(pk=pro_id)
+
+                # Update the OrderList instance with fetched instances
+                ord.ord_price = p
+                ord.ord_qty = q
+                ord.inv = invoice
+                ord.prod = product
+                ord.save()
+
+                obj = {
+                    'code': 200,
+                    'message': 'Order is successfully updated!' if ord else 'Error!',
+                }
+                return JsonResponse(obj)
+
+    return redirect('/')
