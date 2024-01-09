6c6af4d48f980418093b65ef30bb225f5839377a
topecnz <topecnz@gmail.com>
2024-01-10 01:06:50 +0800

    Add reset password feature via session on specific browser

--- inventory/views.py
+++ inventory/views.py
@@ -3,10 +3,15 @@ from django.http import HttpRequest
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.core.mail import EmailMessage
+from django.utils import timezone
 from .forms import *
-from .models import Account
+from .models import Account, Employee
+from eis import settings
 
 import datetime
+import uuid
 
 def user_login(request: HttpRequest):    
     if request.method == 'POST':
@@ -67,3 +72,88 @@ def user_logout(request: HttpRequest):
     if request.user.is_authenticated:
         logout(request)
         return redirect('/login')
+    
+def reset_password(request: HttpRequest):
+    # if not request.user.is_authenticated:
+    if request.method == "POST":
+        emp = Employee.objects.filter(emp_email=request.POST['email']).first()
+        
+        if emp:
+            user = Account.objects.select_related('emp').filter(emp_id=emp.emp_id).first()
+            
+            token = uuid.uuid4()
+            from_email = settings.EMAIL_HOST_USER
+            to_email = user.emp.emp_email
+            url = request.build_absolute_uri(f'/changepass/{token}')
+            
+            email = EmailMessage(
+                'Reset Password',
+                f"Hi!,\n\n Here's the reset password link: {url}",
+                from_email,
+                [to_email]
+            )
+            
+            email.send(fail_silently=False)
+            
+            if email:
+                request.session['r_token'] = str(token)
+                request.session['r_user'] = user.acc_id
+                request.session.modified = True
+            
+        obj = {
+            'success': True if emp and email else False,
+            'status': 'success' if emp and email else 'warning',
+            'message': 'We\'ve sent a reset password link into the associated email.' if emp and email else 'Email not found',
+            'reset': True
+        }
+        
+        return render(request, 'main/reset_password.html', obj)
+    
+    return render(request, 'main/reset_password.html', { 'reset': True })
+    
+    # return redirect('/')
+
+def change_password(request: HttpRequest, token: str):
+    # if not request.user.is_authenticated:
+    is_valid = False
+    if token == request.session.get('r_token'):
+        is_valid = True
+        
+        if request.method == "POST":
+            user = Account.objects.get(acc_id=request.session.get('r_user'))
+            
+            if user:
+                password = request.POST['pass']
+                cpassword = request.POST['cpass']
+                
+                if password != cpassword:
+                    obj = {
+                        'reset': True,
+                        'is_valid': is_valid,
+                        'status': 'warning',
+                        'success': False,
+                        'message': 'Password did not matched!'
+                    }
+                    return render(request, 'main/change_password.html', obj)
+                
+                user.password = make_password(password)
+                user.acc_updated_at = timezone.now()
+                user.save()
+                
+                del request.session['r_token']
+                del request.session['r_user']
+            
+            obj = {
+                'reset': True,
+                'is_valid': is_valid,
+                'status': 'success' if user else 'warning',
+                'success': True if user else False,
+                'message': 'Password has been updated!\nYou can now login now!' if user else 'User not found'
+            }
+            
+            
+            return render(request, 'main/change_password.html', obj)
+
+        return render(request, 'main/change_password.html', { 'reset': True })
+        
+    # return redirect('/')
